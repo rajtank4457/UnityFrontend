d1d8af6c2b5609ef74bc8646d3e98ec9a7f0a468
rajtank4457 <145446813+rajtank4457@users.noreply.github.com>
2024-01-12 15:49:12 +0530

    frontend commited

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+'use client'
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+
+export default function Home() {
+  return (
+   <div >
+   <Header/>
+   <Footer/>
+   </div>
+  )
+}
